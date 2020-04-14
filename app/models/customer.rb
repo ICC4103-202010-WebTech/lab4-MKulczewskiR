411dcaf67209c10c9a44c945aea7ef36bdaa670d
MKulczewskiR <43651599+MKulczewskiR@users.noreply.github.com>
2020-04-13 22:27:23 -0400

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :name, presence: true
+  validates :lastname, presence: true
 end
