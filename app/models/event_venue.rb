e529706b56f1210087b2ade5ec27316b93d4649d
MKulczewskiR <43651599+MKulczewskiR@users.noreply.github.com>
2020-04-13 22:28:00 -0400

    Update event_venue.rb

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,3 +1,7 @@
 class EventVenue < ApplicationRecord
   has_many :events
+  has_many :events
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :capacity, numericality: { only_integer: true, greater_than: 10 }
 end
